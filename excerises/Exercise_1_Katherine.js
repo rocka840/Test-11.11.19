1e5963562bf1c10e75dfaa41caa35260b45cb627
rocka840 <you@example.com>
2019-11-11 11:19:53 +0100

    HMG

---
+++ excerises/Exercise_1_Katherine.js
@@ -0,0 +1,12 @@
+var A = prompt("Give me number A:");
+var B = prompt("Give me number B:");
+
+var a = Number(A);
+var b = Number(B);
+
+
+
+
+console.log(A);
+console.log(B);
+var A = document.getElementById("Output");
---
+++ excerises/Exercise_3_Katherine.js
@@ -0,0 +1,14 @@
+var x = 0;
+var y = 1;
+var m = 0;
+
+for (i == 0;; i++); {
+    m = x + y;
+    x = y;
+    y = m;
+}
+for (x < 1000000) {
+    console.log("There are" + i + "fibonacci numbers");
+}
+
+console.log(x);
